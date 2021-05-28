ec79a4b75a67c8fad167ad571fb9593adbc1d374
Damian Postaru <postaru.damian@gmail.com>
2021-05-28 19:28:26 +0300

    Add pizza client

--- pizza-server/src/main/java/io/damian/pizzaserver/config/PizzaConfig.java
+++ pizza-server/src/main/java/io/damian/pizzaserver/config/PizzaConfig.java
@@ -1,27 +1,24 @@
 package io.damian.pizzaserver.config;
 
-//import io.damian.pizzaserver.model.Pizza;
-//import io.damian.pizzaserver.repository.PizzaRepository;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Configuration
-//public class PizzaConfig {
-//
-////    @Bean
-////    CommandLineRunner commandLineRunner(PizzaRepository pizzaRepository) {
-////        return args -> {
-//////            List<String> toppings = new ArrayList<>();
-//////            toppings.add("Mozzarella");
-//////            toppings.add("Tomatoes");
-//////            toppings.add("Basil");
-//////            Pizza margarita = new Pizza(0L, "margarita", true, 12.5, toppings);
-//////
-//////            pizzaRepository.saveAll(List.of(margarita));
-////        };
-////    }
-//}
+import io.damian.pizzaserver.model.Pizza;
+import io.damian.pizzaserver.repository.PizzaRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class PizzaConfig {
+
+    CommandLineRunner commandLineRunner(PizzaRepository repository) {
+        return args -> {
+            if (repository.findAll().size() == 0) {
+                Pizza margarita = new Pizza(1L, "margarita", true, 12.5, List.of("Mozzarella", "Tomatoes", "Basil"));
+                Pizza fourCheese = new Pizza(2L, "four cheese", true, 12.5, List.of("Crème fraîche", "Mozzarella", "Goat cheese", "Emmental", "Gorgonzola"));
+                Pizza bbqChickenBacon = new Pizza(3L, "bbq chicken & bacon", false, 11.5, List.of("BBQ sauce", "Mozzarella", "Grilled chicken", "Bacon"));
+
+                repository.saveAll(List.of(margarita, fourCheese, bbqChickenBacon));
+            }
+        };
+    }
+}
